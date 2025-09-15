da080158b4d657fbc95a51287c2451b10150c8d5
Tsaroucha Maria <mtsaroucha@intracom-telecom.com>
2025-09-15 17:04:55 +0300

    Initial commit

---
+++ src/main/java/com/maria/ocp21/Ocp21Application.java
@@ -0,0 +1,13 @@
+package com.maria.ocp21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ocp21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ocp21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/maria/ocp21/Ocp21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maria.ocp21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ocp21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
